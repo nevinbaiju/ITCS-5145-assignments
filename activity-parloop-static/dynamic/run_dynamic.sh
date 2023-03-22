39ea3592ca252b9e32b266dcd587044af5d0d1bd
Nevin Baiju <nevinbaiju@gmail.com>
2023-03-22 17:02:44 -0400

    revert run dynamic

--- activity-parloop-static/dynamic/run_dynamic.sh
+++ activity-parloop-static/dynamic/run_dynamic.sh
@@ -34,26 +34,24 @@ SYNCS="thread chunk"
 # ensure sequential data exists before running 
 # assumes the $INTENSITIES and $NS are the same in bench_sequential.sh
 
-for THREAD in $THREADS;
+
+echo THREAD=${THREAD}: before starting time is $(date)
+
+for intensity in $INTENSITIES;
 do
-    echo THREAD=${THREAD}: before starting time is $(date)
-
-    for intensity in $INTENSITIES;
+    for n in $NSPLOT;
     do
-        for n in $NSPLOT;
-        do
-            for gran in $GRANS;
-            do
-    #		    if [ ${gran} -le 100 ] && [ ${n} -ge 1000000  ] ;
-    #:                    then 
-    #		        echo ${gran} ${n}  #> ${RESULTDIR}/dynamic_${n}_${intensity}_${thread}_${sync}_${gran} 
-    #		    else
-    #		qsub -q mamba -d $(pwd) -l nodes=1:ppn=${thread} -v ITENSITY=${intensity},N=${n},THREAD=${thread},SYNC=${sync},GRAN=${gran} run_dynamic.sh
-                    ./dynamic_sched 1 0 10 ${n} ${intensity} ${THREAD} ${gran} 2>${RESULTDIR}/dynamic_${n}_${intensity}_${THREAD}_${gran}  >/dev/null 
-    #		    fi 
-            done
-        done
+		for gran in $GRANS;
+		do
+#		    if [ ${gran} -le 100 ] && [ ${n} -ge 1000000  ] ;
+#:                    then 
+#		        echo ${gran} ${n}  #> ${RESULTDIR}/dynamic_${n}_${intensity}_${thread}_${sync}_${gran} 
+#		    else
+#		qsub -q mamba -d $(pwd) -l nodes=1:ppn=${thread} -v ITENSITY=${intensity},N=${n},THREAD=${thread},SYNC=${sync},GRAN=${gran} run_dynamic.sh
+		        ./dynamic_sched 1 0 10 ${n} ${intensity} ${THREAD} ${gran} 2>${RESULTDIR}/dynamic_${n}_${intensity}_${THREAD}_${gran}  >/dev/null 
+#		    fi 
+	    done
     done
-
-    echo THREAD=${THREAD}: after ending time is $(date)
 done
+
+echo THREAD=${THREAD}: after ending time is $(date)
