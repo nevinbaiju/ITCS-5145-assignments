bef45a1db939048e49f63489e1b72bcfd1edf318
Nevin Baiju <nevinbaiju@gmail.com>
2023-04-05 22:29:23 -0400

    commented out sequential code

--- activity-mergesort/mergesort/mergesort.cpp
+++ activity-mergesort/mergesort/mergesort.cpp
@@ -24,18 +24,14 @@ void merge(int * arr, int l, int m, int r)
     int n1 = m - l + 1;
     int n2 =  r - m;
  
-    /* create temp arrays */
-    //int L[n1], R[n2];
     std::vector<int> L (n1);
     std::vector<int> R(n2);
 
-    /* Copy data to temp arrays L[] and R[] */
     for (i = 0; i < n1; i++)
         L[i] = arr[l + i];
     for (j = 0; j < n2; j++)
         R[j] = arr[m + 1+ j];
  
-    /* Merge the temp arrays back into arr[l..r]*/
     i = 0;
     j = 0;
     k = l;
@@ -55,7 +51,6 @@ void merge(int * arr, int l, int m, int r)
         k++;
     }
  
-    /* Copy the remaining elements of L[], if there are any */
     while (i < n1)
     {
         arr[k] = L[i];
@@ -63,7 +58,6 @@ void merge(int * arr, int l, int m, int r)
         k++;
     }
  
-    /* Copy the remaining elements of R[], if there are any */
     while (j < n2)
     {
         arr[k] = R[j];
@@ -84,14 +78,9 @@ void print_arr(int * arr, int start, int end){
 
 void mergesort(int * arr, int n, int num_threads)
 {
-   int curr_size;  // For current size of subarrays to be merged
-                   // curr_size varies from 1 to n/2
-   int left_start; // For picking starting index of left subarray
-                   // to be merged
- 
-   // Merge subarrays in bottom up manner.  First merge subarrays of
-   // size 1 to create sorted subarrays of size 2, then merge subarrays
-   // of size 2 to create sorted subarrays of size 4, and so on.
+   int curr_size;
+   int left_start;
+   
    OmpLoop parloop;
    parloop.setNbThread(num_threads);
    parloop.setGranularity(1);
@@ -99,39 +88,33 @@ void mergesort(int * arr, int n, int num_threads)
    bool break_condition = false;
    for (curr_size=1; curr_size<=n-1; curr_size = 2*curr_size)
    {
-      // std::cout << curr_size << std::endl;
-      // parloop.parfor(0, n-1, 2*curr_size, 
-      //               [&](int left_start){
-      //                   int mid = std::min(left_start + curr_size - 1, n-1);
-      //                   int right_end = std::min(left_start + 2*curr_size - 1, n-1);
-      //                   print_arr(arr, left_start, mid);
-      //                   print_arr(arr, mid+1, right_end);
-      //                   // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
-      //                   merge(arr, left_start, mid, right_end);
+      parloop.parfor(0, n-1, 2*curr_size, 
+                    [&](int left_start){
+                        int mid = std::min(left_start + curr_size - 1, n-1);
+                        int right_end = std::min(left_start + 2*curr_size - 1, n-1);
+                        print_arr(arr, left_start, mid);
+                        print_arr(arr, mid+1, right_end);
+                        // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
+                        merge(arr, left_start, mid, right_end);
 
-      //                   print_arr(arr, left_start, right_end);
-      //               });
-      //  Pick starting point of different subarrays of current size
-      // std::cout << "Curr size: " << curr_size << std::endl; 
-       for (left_start=0; left_start<n-1; left_start += 2*curr_size)
-       {
-          if (left_start >= n){
-            break_condition = true;
-            break;
-          }
-           // Find ending point of left subarray. mid+1 is starting
-           // point of right
-           int mid = std::min(left_start + curr_size - 1, n-1);
+                        print_arr(arr, left_start, right_end);
+                    });
+      //  for (left_start=0; left_start<n-1; left_start += 2*curr_size)
+      //  {
+      //     if (left_start >= n){
+      //       break_condition = true;
+      //       break;
+      //     }
+      //      int mid = std::min(left_start + curr_size - 1, n-1);
  
-           int right_end = std::min(left_start + 2*curr_size - 1, n-1);
+      //      int right_end = std::min(left_start + 2*curr_size - 1, n-1);
 
-           print_arr(arr, left_start, mid);
-           print_arr(arr, mid+1, right_end);
-           // Merge Subarrays arr[left_start...mid] & arr[mid+1...right_end]
-           merge(arr, left_start, mid, right_end);
-           merge_counter++;
-           print_arr(arr, left_start, right_end);
-       }
+      //      print_arr(arr, left_start, mid);
+      //      print_arr(arr, mid+1, right_end);
+      //      merge(arr, left_start, mid, right_end);
+      //      merge_counter++;
+      //      print_arr(arr, left_start, right_end);
+      //  }
        if (break_condition){
         break;
        }
