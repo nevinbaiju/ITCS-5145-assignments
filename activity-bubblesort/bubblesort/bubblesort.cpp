97e56fc340a7aabf8e27f7196c0fc2b97553be0f
Nevin Baiju <nevinbaiju@gmail.com>
2023-04-05 21:17:18 -0400

    Bubblesort

--- activity-bubblesort/bubblesort/bubblesort.cpp
+++ activity-bubblesort/bubblesort/bubblesort.cpp
@@ -5,6 +5,10 @@
 #include <iostream>
 #include <unistd.h>
 #include "omploop.hpp"
+#include <chrono>
+
+#define DEBUG 0
+#define PARALLEL 1
 
 #ifdef __cplusplus
 extern "C" {
@@ -16,6 +20,12 @@ extern "C" {
 #ifdef __cplusplus
 }
 #endif
+
+void swap(int* arr, int i, int j) {
+  int temp = arr[i];
+  arr[i] = arr[j];
+  arr[j] = temp;
+}
 
 
 int main (int argc, char* argv[]) {
@@ -29,8 +39,74 @@ int main (int argc, char* argv[]) {
   int * arr = new int [n];
   generateMergeSortData (arr, n);
 
-  //insert sorting code here.
+  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  OmpLoop parloop;
+  parloop.setGranularity(std::max(10, n/100));
+  for (int i=0; i<n; i++){
+    for (int j=0; j<2; j++){
+      #if PARALLEL
+        parloop.parfor(i, 0, -1, 
+                      [&](int k){
+                        int a = j+(i-k)*2;
+                        if(a < n-1){
+                          #if DEBUG
+                            std::cout << "(" << k << "," << a << ")" << ", ";
+                          #endif
+                          if (arr[a] > arr[a+1]){
+                            swap(arr, a, a+1);
+                          }
+                        }
+                      });
+      #else 
+        for(int k=i; k>=0; k--){
+          int a = j + (i-k)*2;
+          if(a < n-1){
+            #if DEBUG
+              std::cout << "(" << k << "," << a << ")" << ", ";
+            #endif
+            if (arr[a] > arr[a+1]){
+              swap(arr, a, a+1);
+            }
+          }
+        }
+        #if DEBUG
+          std::cout << "\n";
+        #endif
+      #endif
+    }
+  }
+  for(int i=2; i<n-1; i++){
+    for(int k=n-1; k>=0; k--){
+        int a = i + ((n-1)-k)*2;
+        if(a < n-1){
+          #if DEBUG
+            std::cout << "(" << k << "," << a << ")" << ", ";
+          #endif
+          if (arr[a] > arr[a+1]){
+            swap(arr, a, a+1);
+          }
+        }
+    }
+  }
 
+
+  
+  // for(int i=0; i<n; i++){
+  //   for(int j=0; j<n-1; j++){
+  //     #if DEBUG
+  //       std::cout << "(" << i << "," << j << ")" << ", ";
+  //     #endif
+  //     if(arr[j] > arr[j+1]){
+  //       swap(arr, j, j+1);
+  //     }
+  //   }
+  //   #if DEBUG
+  //     std::cout << "\n";
+  //   #endif
+  // }
+  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::duration<double> elpased_seconds = end-start;
+  std::cerr<<elpased_seconds.count()<<std::endl;
 
   
   checkMergeSortResult (arr, n);
