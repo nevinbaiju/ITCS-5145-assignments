871b24432933257fdb6f2c687904180f743a4807
Nevin Baiju <nevinbaiju@gmail.com>
2023-04-10 15:55:33 -0400

    Added code

--- assignment-mpi-hello-world/hello_world/hello.cpp
+++ assignment-mpi-hello-world/hello_world/hello.cpp
@@ -3,5 +3,18 @@
 #include <iostream>
 
 int main(int argc, char*argv[]) {
-
+    MPI_Init (&argc, &argv);
+    
+    int size;
+    int rank;
+    
+    MPI_Comm_size(MPI_COMM_WORLD, &size);
+    MPI_Comm_rank(MPI_COMM_WORLD, &rank);
+    
+    sleep(rank);
+    
+    std::cout<<"I am "<<rank<<" out of "<<size<<"\n";
+    std::cout<<"I am unix process :"<<getpid()<<"\n";
+    
+    MPI_Finalize();
 }
