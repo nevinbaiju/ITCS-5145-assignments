d4f3f314c79f48a671e5e02dde7e065b307fbe53
Nevin Baiju <nevinbaiju@gmail.com>
2023-04-17 21:34:46 -0400

    mpi manager worker passed on local

--- assignment-mpi-manager-worker/manager_worker/mpi_manager_worker.cpp
+++ assignment-mpi-manager-worker/manager_worker/mpi_manager_worker.cpp
@@ -4,10 +4,14 @@
 #include <chrono>
 #include <mpi.h>
 #include <vector>
+#include <chrono>
 
 #ifdef __cplusplus
 extern "C" {
 #endif
+
+#define GRANULARITY 100
+#define DEBUG 0
 
 float f1(float x, int intensity);
 float f2(float x, int intensity);
@@ -18,20 +22,85 @@ float f4(float x, int intensity);
 }
 #endif
 
-float compute (float (*func)(float, int), float a, float b, int n, int intensity, int rank, int partition_size){
-  int start = partition_size*rank;
-  int end = std::min(n, partition_size*(rank+1));
-  #if DEBUG
-    std::cout << "Rank " << rank << " "<< start << ":" << end << " Partition " << partition_size << "\n";
-  #endif
-  float b_minus_a_over_n = (b - a)/n;
-  float ans = 0;
-  for (int i = start; i<end; i++){
-    ans = ans + func(a + (i + 0.5)*b_minus_a_over_n, intensity);
-  }
-  ans = ans;
+void compute (float (*func)(float, int), float a, float b, int n, int intensity, int rank, int size){
+  std::vector<int> start_end(2);
+  std::vector<int> status(1);
+  std::vector<float> ans(1);
+  std::vector<float> curr_ans(1);
 
-  return ans;
+  MPI_Status s;
+
+  if(rank ==0){
+    ans[0] = 0;
+    int current_communicator = 1;
+    for(int i=0; i<n; i+=GRANULARITY){
+      #if DEBUG
+        std::cout << "i= " << i << " "<< i+GRANULARITY << "\n";
+      #endif
+      if (current_communicator == size){
+        for(int node=1; node<size; node++){
+          MPI_Recv(&(curr_ans[0]), 2, MPI_INT, node, 69, MPI_COMM_WORLD, &s);
+          #if DEBUG
+            std::cout << "Receiving from Rank " << node << " "<< curr_ans[0] << "\n";
+          #endif
+          ans[0] = ans[0] + curr_ans[0];
+        }
+        current_communicator=1;
+      }
+      
+      start_end[0] = i;
+      start_end[1] = std::min(n, i+GRANULARITY);
+      status[0] = 1;
+      #if DEBUG
+        std::cout << "Sending to Rank " << current_communicator << " "<< start_end[0] << ":" << start_end[1] << "\n";
+      #endif
+      MPI_Send(&(status[0]), 2, MPI_INT, current_communicator, 100, MPI_COMM_WORLD);
+      MPI_Send(&(start_end[0]), 2, MPI_INT, current_communicator, 123, MPI_COMM_WORLD);
+      current_communicator++;
+      
+    }
+    #if DEBUG
+          std::cout << "Iterations complete" << "\n";
+    #endif
+    for(int node=1; node<current_communicator; node++){
+      #if DEBUG
+        std::cout << "Receiving from Rank " << node << " "<< curr_ans[0] << "\n";
+      #endif
+      MPI_Recv(&(curr_ans[0]), 2, MPI_INT, node, 69, MPI_COMM_WORLD, &s);
+      ans[0] = ans[0] + curr_ans[0];
+    }
+    
+    for(int node=1; node<size; node++){
+      status[0] = 0;
+      #if DEBUG
+        std::cout << "Sending signal to shut down " << node << "\n";
+      #endif
+      MPI_Send(&(status[0]), 2, MPI_INT, node, 100, MPI_COMM_WORLD);
+    }
+
+    #if DEBUG
+      std::cout << "Shutting down " << "Root" << "\n";
+    #endif
+    std::cout << ans[0]*((b - a)/n) << std::endl;
+  }
+  else{
+    MPI_Recv(&(status[0]), 2, MPI_INT, 0, 100, MPI_COMM_WORLD, &s);
+    while(status[0]){
+      MPI_Recv(&(start_end[0]), 2, MPI_INT, 0, 123, MPI_COMM_WORLD, &s);
+      ans[0] = 0;
+      for (int i = start_end[0]; i<start_end[1]; i++){
+        ans[0] = ans[0] + func(a + (i + 0.5)*((b - a)/n), intensity);
+      }
+      MPI_Send(&(ans[0]), 2, MPI_INT, 0, 69, MPI_COMM_WORLD);
+      #if DEBUG
+          std::cout << "Rank " << rank << " "<< start_end[0] << ":" << start_end[1] << " Partition "<< n<< "\n";
+      #endif
+      MPI_Recv(&(status[0]), 2, MPI_INT, 0, 100, MPI_COMM_WORLD, &s);
+    }
+    #if DEBUG
+      std::cout << "Shutting down " << rank << "\n";
+    #endif
+  }
 
 }
 
@@ -62,47 +131,42 @@ int main (int argc, char* argv[]) {
   std::vector<int> ans(1);
   std::vector<int> final_ans(1);
 
-  auto start = chrono::steady_clock::now();
+  auto start = std::chrono::steady_clock::now();
 
   switch(function_id) {
     case 1:{
-      ans[0] = compute(&f1, a, b, n, intensity, rank, partition_size);
-      MPI_Reduce(&(ans[0]), &(final_ans[0]), 1, MPI_INT,
-                MPI_SUM, 0, MPI_COMM_WORLD);
-      if(rank == 0){
-        cout << final_ans[0]*((b - a)/n) << "\n";
-      }
+      compute(&f1, a, b, n, intensity, rank, size);
       break;
     }
 
-    case 2:{
-      float ans = compute(&f2, a, b, n, intensity, rank, partition_size);
-      cout << ans << "\n";
-      break;
-    }
+    // case 2:{
+    //   float ans = compute(&f2, a, b, n, intensity, rank, partition_size);
+    //   std::cout << ans << "sdfsadfsd\n";
+    //   break;
+    // }
 
-    case 3:{
-      float ans = compute(&f3, a, b, n, intensity, rank, partition_size);
-      cout << ans << "\n";
-      break;
-    }
+    // case 3:{
+    //   float ans = compute(&f3, a, b, n, intensity, rank, partition_size);
+    //   std::cout << ans << "\n";
+    //   break;
+    // }
 
-    case 4:{
-      float ans = compute(&f4, a, b, n, intensity, rank, partition_size);
-      cout << ans << "\n";
-      break;
-    }
+    // case 4:{
+    //   float ans = compute(&f4, a, b, n, intensity, rank, partition_size);
+    //   std::cout << ans << "\n";
+    //   break;
+    // }
     default:
-    cout << "Wrong function ID";
+    std::cout << "Wrong function ID";
   }
 
-  auto end = chrono::steady_clock::now();
+  auto end = std::chrono::steady_clock::now();
   double time_taken = std::chrono::duration_cast<std::chrono::nanoseconds>(end - start).count() / 1e9;
 
   MPI_Finalize();
 
   if(rank==0){
-    cerr << time_taken;
+    std::cerr << time_taken;
   }
   return 0;
 
